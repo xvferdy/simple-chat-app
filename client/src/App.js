f56a920330904749c9fc9d07a6f1a11588cf521f
Berlianto Ferdynand Pongbubun <xv.ferdy@gmail.com>
2021-11-14 15:27:59 +0800

    Design (#1)
    * To the moon
    
    * ♻️ Try to deploy
    
    refactor css
    
    * Refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,64 @@
-import "./App.css";
 import { useState } from "react";
 import io from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
 import Chat from "./Chat";
+// import "./App.css";
+import "./stylesheets/css/main.css";
 
 const socket = io.connect("http://localhost:8000");
 
-//socket.io
+// socket.io
 // .on
 // .emit
 // .join
 // .to
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [userId, setUserId] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
+	const [username, setUsername] = useState("");
+	const [userId, setUserId] = useState("");
+	const [room, setRoom] = useState("");
+	const [showChat, setShowChat] = useState(false);
 
-  const joinRoom = async (e) => {
-    e.preventDefault();
-    if (username !== "" && room !== "") {
-      //someone want to join (join_room event)
-      await socket.emit("join_room", room);
-      setUserId(uuidv4());
-      setShowChat(true);
-    } else {
-      alert("Please enter Username and Room ID");
-    }
-  };
-  return (
-    <div className="App">
-      {showChat ? (
-        <Chat socket={socket} username={username} room={room} userId={userId} />
-      ) : (
-        <form>
-          <div className="joinChatContainer">
-            <h1>Join Chat</h1>
-            <input
-              type="text"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-              placeholder="Username"
-              autoFocus
-            />
-            <input
-              type="text"
-              onChange={(e) => {
-                setRoom(e.target.value);
-              }}
-              placeholder="Room ID"
-            />
-            <button onClick={joinRoom}>Enter</button>
-          </div>
-        </form>
-      )}
-    </div>
-  );
+	const joinRoom = async (e) => {
+		e.preventDefault();
+		if (username !== "" && room !== "") {
+			//someone want to join (join_room event)
+			await socket.emit("join_room", room);
+			setUserId(uuidv4());
+			setShowChat(true);
+		} else {
+			alert("Please enter Username and Room ID");
+		}
+	};
+	return (
+		<div className="App">
+			{showChat ? (
+				<Chat socket={socket} username={username} room={room} userId={userId} />
+			) : (
+				<form>
+					<div className="joinChatContainer">
+						<h1>Join Chat</h1>
+						<input
+							type="text"
+							onChange={(e) => {
+								setUsername(e.target.value);
+							}}
+							placeholder="Username"
+							autoFocus
+						/>
+						<input
+							type="text"
+							onChange={(e) => {
+								setRoom(e.target.value);
+							}}
+							placeholder="Room ID"
+						/>
+						<button onClick={joinRoom}>Enter</button>
+					</div>
+				</form>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -9,36 +9,34 @@ app.use(cors());
 
 //socket.io related
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
 });
 
 //socket.io EVENTS
 io.on("connection", (socket) => {
-  console.log("User Connected:", socket.id);
+	console.log("User Connected:", socket.id);
 
-  //someone want to join
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    // console.log(data);
-  });
+	// someone want to join
+	socket.on("join_room", (data) => {
+		socket.join(data);
+	});
 
-  //someone chating
-  socket.on("send_message", (data) => {
-    //emit to frontend
-    // data => object of message detail
-    socket.to(data.room).emit("receive_message", data);
-    // console.log(data);
-  });
+	// someone chating
+	socket.on("send_message", (data) => {
+		// emit to frontend
+		// data => object of message detail
+		socket.to(data.room).emit("receive_message", data);
+	});
 
-  //disconnect
-  socket.on("disconnect", () => {
-    console.log("User Disconnected:", socket.id);
-  });
+	// disconnect
+	socket.on("disconnect", () => {
+		console.log("User Disconnected:", socket.id);
+	});
 });
 
 server.listen(8000, () => {
-  console.log("PORT RUNNING IN 8000");
+	console.log("PORT RUNNING IN 8000");
 });
