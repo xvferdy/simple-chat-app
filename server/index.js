a77b452eaaf79de0f0249db32789ace9599c50e5
Berlianto Ferdynand Pongbubun <xv.ferdy@gmail.com>
2022-12-23 18:46:02 +0700

    🚑️ [hotfix]: host server on another free platform (#8)
    railway.app

--- server/index.js
+++ server/index.js
@@ -12,34 +12,34 @@ app.use(router);
 const port = process.env.PORT || 8000;
 
 const io = new Server(server, {
-	cors: {
-		// origin: "http://localhost:3000",
-		origin: "https://simple-chat-app-xvferdy.netlify.app",
-		methods: ["GET", "POST"],
-	},
+  cors: {
+    // origin: "http://localhost:3000",
+    origin: "https://simple-chat-app-xvferdy.netlify.app",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-	console.log("User Connected:", socket.id);
+  console.log("User Connected:", socket.id);
 
-	// someone want to join
-	socket.on("join_room", (data) => {
-		socket.join(data);
-	});
+  // someone want to join
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
 
-	// someone chating
-	socket.on("send_message", (data) => {
-		// emit to frontend
-		// data => object of message detail
-		socket.to(data.room).emit("receive_message", data);
-	});
+  // someone chating
+  socket.on("send_message", (data) => {
+    // emit to frontend
+    // data => object of message detail
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-	// disconnect
-	socket.on("disconnect", () => {
-		console.log("User Disconnected:", socket.id);
-	});
+  // disconnect
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
 });
 
 server.listen(port, () => {
-	console.log("Server is up and running!");
+  console.log("Server is up and running!");
 });
